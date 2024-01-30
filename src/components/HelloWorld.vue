7b03a07b2b8704cda236dde60a9f2b89e22aa8bf
Rustam <rustam.radzabov8410@gmail.com>
2024-01-30 16:51:37 +0500

    install dependences

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,38 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
+<script>
+export default {
+  data: () => ({
+    cursors: [
+      'auto',
+      'default',
+      'grab',
+      'grabbing',
+      'help',
+      'move',
+      'none',
+      'not-allowed',
+      'pointer',
+      'progress',
+      'text',
+      'wait',
+    ],
+  }),
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <v-container>
+    <v-row justify="space-between">
+      <v-col v-for="cursor in cursors" :key="cursor" cols="3">
+        <v-btn
+            :text="cursor"
+            :class="`cursor-${cursor}`"
+            block
+        ></v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+
 </style>
