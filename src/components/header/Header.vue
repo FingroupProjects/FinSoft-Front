00c410a407dd688cd0d4ddd8ca13265fd8d977c0
Rustam <rustam.radzabov8410@gmail.com>
2024-02-01 12:03:48 +0500

    header

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,21 +1,106 @@
 <script setup>
 
+import {ref} from "vue";
+
+const dialog = ref(false)
 </script>
 
 <template>
-  <v-app-bar :elevation="6">
+  <v-app-bar
+      :elevation="2"
+      color="info"
+      height="45"
+  >
     <template v-slot:prepend>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-icon size="18" class="text-grey-lighten-3">home</v-icon>
+      <v-icon size="12" class="ms-1">chevron_right</v-icon>
     </template>
 
-    <v-app-bar-title>Application Bar</v-app-bar-title>
+    <v-app-bar-title class="text-sm-body-1">Клиент</v-app-bar-title>
 
     <template v-slot:append>
-      <v-btn icon="mdi-heart"></v-btn>
+      <v-btn density="comfortable" variant="tonal" color="info" class="d-flex justify-space-between ga-3  text-sm-body-2 bg-white rounded-lg py-1 cursor-pointer">
+        <template v-slot:prepend>
+          <v-icon>search</v-icon>
+          <span class="ms-2">Поиск...</span>
+        </template>
 
-      <v-btn icon="mdi-magnify"></v-btn>
+        <template v-slot:append>
+          <span class="bg-blue rounded-lg px-1">Ctrl+K</span>
+        </template>
 
-      <v-btn icon="mdi-dots-vertical"></v-btn>
+        <v-dialog
+            v-model="dialog"
+            activator="parent"
+            width="400"
+            class="rounded-lg"
+        >
+          <v-card>
+            <v-card-title>
+              <div class="d-flex align-center">
+                <v-icon>search</v-icon>
+                <v-text-field
+                    placeholder="Поиск...."
+                    variant="plain"
+                >
+                </v-text-field>
+              </div>
+            </v-card-title>
+            <v-divider/>
+            <v-card-text>
+              <v-container>
+
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                  color="blue-darken-1"
+                  variant="text"
+                  @click="dialog = false"
+              >
+                Close
+              </v-btn>
+              <v-btn
+                  color="blue-darken-1"
+                  variant="text"
+                  @click="dialog = false"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-btn>
+
+      <v-btn variant="text" size="55" class="ms-2" >
+        <v-icon size="20">add_circle_outline</v-icon>
+        <v-icon size="20">expand_more</v-icon>
+      </v-btn>
+
+      <v-btn variant="text" size="55" >
+        <v-icon size="20">help_outline</v-icon>
+        <v-icon size="20">expand_more</v-icon>
+      </v-btn>
+
+      <v-btn variant="text" size="55" >
+        <v-icon size="20">notifications</v-icon>
+        <v-icon size="20">expand_more</v-icon>
+      </v-btn>
+
+      <v-btn variant="text" class="d-flex justify-space-between">
+        <v-icon size="20">language</v-icon>
+        <span class="text-sm-body-2 mx-2">Русский</span>
+        <v-icon size="20">expand_more</v-icon>
+      </v-btn>
+
+      <v-btn variant="text" class="d-flex justify-space-between" rounded="2">
+        <v-icon size="20">account_circle</v-icon>
+        <span class="text-sm-body-2 mx-2">Hello</span>
+        <v-icon size="20">expand_more</v-icon>
+      </v-btn>
+
     </template>
   </v-app-bar>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,27 +1,14 @@
 import { createApp } from 'vue'
-import { createVuetify } from 'vuetify'
 import { createPinia } from 'pinia'
 import router from './router/index.js'
+import vuetify from "./vuetify.js";
 import App from './App.vue'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 import 'vuetify/styles'
-
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import { aliases, md } from 'vuetify/iconsets/md'
+
 
 const pinia = createPinia()
-const vuetify = createVuetify({
-  components,
-  directives,
-  icons: {
-    defaultSet: 'md',
-    aliases,
-    sets: {
-      md,
-    },
-  },
-})
+
 
 createApp(App)
   .use(vuetify)
