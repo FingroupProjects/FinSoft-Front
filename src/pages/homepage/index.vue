2f236ddab1094c74f881e16c2032d35c2caab38a
john_front_dev <jamshid200404@gmail.com>
2024-02-03 15:12:56 +0500

    pages

--- src/pages/homepage/index.vue
+++ src/pages/homepage/index.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div>
-    The Main pagedasddddddddddddddddd
+    The Main page
   </div>
 </template>
 
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,7 +3,52 @@ const routes = [
     path: '/',
     name: 'main',
     component: () => import('../pages/homepage/index.vue')
-  }
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    component: () => import('../pages/planning/index.vue')
+  },
+  {
+    path: '/procurementOfGoods',
+    name: 'procurementOfGoods',
+    component: () => import('../pages/procurementOfGoods/index.vue')
+  },
+  {
+    path: '/sellingGoods',
+    name: 'sellingGoods',
+    component: () => import('../pages/sellingGoods/index.vue')
+  },
+  {
+    path: '/sklad',
+    name: 'sklad',
+    component: () => import('../pages/sklad/index.vue')
+  },
+  {
+    path: '/cash',
+    name: 'cash',
+    component: () => import('../pages/cash/index.vue')
+  },
+  {
+    path: '/',
+    name: 'main',
+    component: () => import('../pages/homepage/index.vue')
+  },
+  {
+    path: '/',
+    name: 'main',
+    component: () => import('../pages/homepage/index.vue')
+  },
+  {
+    path: '/',
+    name: 'main',
+    component: () => import('../pages/homepage/index.vue')
+  },
+  {
+    path: '/',
+    name: 'main',
+    component: () => import('../pages/homepage/index.vue')
+  },
 ]
 
 export default routes
