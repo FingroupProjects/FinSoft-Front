28ff74af0ad92eb8e324eb39c55144adafd06984
Rustam <rustam.radzabov8410@gmail.com>
2024-02-08 15:13:13 +0500

    'sv'

--- src/App.vue
+++ src/App.vue
@@ -62,7 +62,7 @@ watch(route, (newVal) => {
   -webkit-box-align: center;
   align-items: center;
   position: relative;
-  background-image: url('auth/login-bg.jpg');
+  background-image: url('assets/svg/auth/login-bg.jpg');
   background-repeat: initial;
   background-attachment: initial;
   background-origin: initial;
