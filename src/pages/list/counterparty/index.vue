b0bb73ef3773ce62ce90452bb719cbb2a677612d
john_front_dev <jamshid200404@gmail.com>
2024-02-09 14:37:13 +0500

    tasks

---
+++ src/pages/list/counterparty/index.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, onMounted } from "vue"
+import counterpartyApi from "../../../api/counterparty"
+
+const fetchCounterparty = async () => {
+  try {
+    const res = await counterpartyApi.getCounterparty()
+    console.log('Контрагент получен:', res)
+  } catch (error) {
+    console.error('Ошибка при получении контрагента:', error)
+  }
+}
+
+onMounted(() => {
+  fetchCounterparty()
+})
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style scoped></style>
