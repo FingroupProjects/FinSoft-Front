524ee93d7728d935e88dba1e700ec106760f56f1
john_front_dev <jamshid200404@gmail.com>
2024-02-14 17:45:28 +0500

    counterparty_validation

--- src/pages/list/counterparty/create.vue
+++ src/pages/list/counterparty/create.vue
@@ -2,16 +2,29 @@
 import { ref, } from "vue"
 import { useRouter } from "vue-router";
 import counterpartyApi from "../../../api/counterparty"
+import showToast from '../../../composables/toast'
 
 const router = useRouter()
+
 const name = ref('')
 const phone = ref('')
 const address = ref('')
 const email = ref('')
 const roles = ref([])
 
+const name_validate = ref(true)
+const phone_validate = ref(false)
+const address_validate = ref(false)
+const email_validate = ref(false)
+
 const CreateCounterparty = async () => {
   try {
+
+    name_validate.value = true
+    phone_validate.value = true
+    address_validate.value = true
+    email_validate.value = true
+
     const body = {
       name: name.value,
       phone: phone.value,
@@ -19,12 +32,46 @@ const CreateCounterparty = async () => {
       email: email.value,
       roles: roles.value,
     }
-    const res = await counterpartyApi.create(body)
+    console.log(body);
+    await counterpartyApi.create(body)
     router.push('counterparty')
+    showToast('Успешно добавлена')
   } catch (error) {
     console.error(error)
   }
 }
+
+const nameRules = ref([
+  value => {
+    if (value) return true
+    return 'Поле не может быть пустым'
+  },
+])
+
+const phoneRules = ref([
+  value => {
+    if (value?.length === 13) return true
+    return 'Введите валидный номер телефона'
+  },
+])
+
+const emailRules = ref([
+  value => {
+    if (value) return true
+    return 'Поле email объязательна'
+  },
+  value => {
+    if (/.+@.+\..+/.test(value)) return true
+    return 'Введите валидную почту'
+  },
+])
+
+const addressRules = ref([
+  value => {
+    if (value) return true
+    return 'Поле не может быть пустым'
+  },
+])
 
 const handleCheckboxChange = (index) => {
   if (roles.value.includes(index + 1)) {
@@ -39,24 +86,25 @@ const handleCheckboxChange = (index) => {
 <template>
   <div>
     <div class="d-flex justify-end mb-4">
-      <v-btn rounded="lg" color="info" @click="$router.push('counterparty')">Назад</v-btn>
+      <v-btn rounded="lg" color="info" @click.prevent="$router.push('counterparty')">Назад</v-btn>
     </div>
     <v-card class="block">
-
-      <div class="d-flex ga-5">
-        <v-text-field variant="outlined" label="Наименование контрагента" v-model="name" />
-        <v-text-field variant="outlined" label="Тел номер" v-model="phone" v-mask="'+992 ### ## ##'" />
-        <v-text-field variant="outlined" label="Адрес" v-model="address" />
-        <v-text-field variant="outlined" type="email" label="Почта" v-model="email" />
-      </div>
-      <div class="d-flex w-75">
-        <v-checkbox-btn label="Клиент" color="info" @change="handleCheckboxChange(0)"></v-checkbox-btn>
-        <v-checkbox-btn label="Поставщик" color="info" @change="handleCheckboxChange(1)"></v-checkbox-btn>
-        <v-checkbox-btn label="Прочие отношения" color="info" @change="handleCheckboxChange(2)"></v-checkbox-btn>
-      </div>
-      <div class="d-flex justify-end mt-4">
-        <v-btn rounded="lg" color="info" @click="CreateCounterparty()">Создать</v-btn>
-      </div>
+      <v-form @submit.prevent="CreateCounterparty">
+        <div class="d-flex ga-5">
+          <v-text-field variant="outlined" :rules="name_validate ? nameRules : []" label="Наименование" v-model="name" />
+          <v-text-field variant="outlined" :rules="phone_validate ? phoneRules : []" label="Тел номер" v-model.trim="phone" v-mask="'+992#########'"/>
+          <v-text-field variant="outlined" :rules="address_validate ? addressRules : []" label="Адрес" v-model="address" />
+          <v-text-field variant="outlined" :rules="email_validate ? emailRules : []" label="Почта" v-model="email" />
+        </div>
+        <div>
+          <v-checkbox-btn label="Клиент" color="info" @change="handleCheckboxChange(0)"></v-checkbox-btn>
+          <v-checkbox-btn label="Поставщик" color="info" @change="handleCheckboxChange(1)"></v-checkbox-btn>
+          <v-checkbox-btn label="Прочие отношения" color="info" @change="handleCheckboxChange(2)"></v-checkbox-btn>
+        </div>
+        <div class="d-flex justify-end mt-4">
+          <v-btn rounded="lg" color="info" type="submit">Создать</v-btn>
+        </div>
+      </v-form>
     </v-card>
   </div>
 </template>
