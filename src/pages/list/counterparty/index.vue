d9eda51cbd388fb33f0a2f9e4d855216d7d9ec5a
john_front_dev <jamshid200404@gmail.com>
2024-02-19 17:32:14 +0500

    counterpartyAgreement

--- src/pages/list/counterparty/index.vue
+++ src/pages/list/counterparty/index.vue
@@ -8,7 +8,10 @@ const loading = ref(true)
 const router = useRouter()
 
 const counterparty = ref([])
+const paginations = ref([])
+
 const headers = ref([
+  { title: '№', key: 'id', align: 'start', },
   { title: 'Наименование', key: 'name', },
   { title: 'Адрес', key: 'address' },
   { title: 'Тип контрагента', key: 'roles' },
@@ -41,23 +44,22 @@ const showDetail = item => {
   router.push({ name: 'detailCounterparty', params: { id: item.id } })
 }
 
-const fetchCounterparty = async () => {
+const fetchCounterparty = async ({ page, itemsPerPage, sortBy }) => {
   try {
-    const { data } = await counterpartyApi.get()
-    counterparty.value = data.result.map(item => ({
+    const { data } = await counterpartyApi.get(page, itemsPerPage, sortBy)
+    console.log(data);
+    counterparty.value = data.result.data.map(item => ({
       ...item,
       created_at: formatDateTime(item.created_at),
       roles: formatRole(item.roles)
     }))
+    paginations.value = data.result.pagination
     loading.value = false
   } catch (error) {
     console.error(error)
   }
 }
 
-onMounted(() => {
-  fetchCounterparty()
-})
 </script>
 <template>
   <div>
@@ -69,25 +71,24 @@ onMounted(() => {
         <v-btn rounded="lg" color="info" @click="$router.push('createCounterparty')">Создать</v-btn>
       </div>
       <v-card class="table">
-        <v-data-table :items="counterparty" :headers="headers" :loading="loading"
-          items-per-page-text="Элементов на странице:" loading-text="Загрузка" no-data-text="Нет данных">
+        <v-data-table-server :items="counterparty" :headers="headers" :loading="loading"
+          items-per-page-text="Элементов на странице:" loading-text="Загрузка" no-data-text="Нет данных"
+          @update:options="fetchCounterparty" v-model:items-per-page="paginations.per_page"
+          :items-length="paginations.total || 0" :item-value="headers.title">
+          <template v-slot:item.id="{ item, index }">
+            <span>{{ index + 1 }}</span>
+          </template>
           <template #item.icons="{ item }">
             <v-icon class="icon mr-2" @click="pushToRename(item)">edit</v-icon>
             <v-icon class="icon" @click="showDetail(item)">visibility</v-icon>
           </template>
-        </v-data-table>
+        </v-data-table-server>
       </v-card>
     </v-col>
   </div>
 </template>
 
 <style scoped>
-.table {
-  background: white;
-  padding: 5px;
-  border-radius: 16px;
-}
-
 .icon {
   opacity: 75%;
 }
---
+++ src/pages/list/counterpartyAgreement/index.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, onMounted } from "vue"
+import { useRouter } from "vue-router"
+import counterpartyAgreement from "../../../api/counterpartyAgreement"
+
+const router = useRouter()
+const result = ref([])
+const getDocuments = async ({ page, itemsPerPage, sortBy }) => {
+  try {
+    const { data } = await counterpartyAgreement.get(page, itemsPerPage, sortBy)
+    result.value = data.result
+    console.log(data);
+  }
+  catch (e) {
+    console.log(e);
+  }
+}
+onMounted(() => {
+  getDocuments()
+})
+
+</script>
+
+<template>
+  <div>
+    
+  </div>
+</template>
+<style scoped></style>
