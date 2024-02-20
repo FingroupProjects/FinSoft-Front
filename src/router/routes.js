fac68c2adb7d0f0c403c9e5f7824478f1588d97b
FTursunboy <tfaiziev04@gmail.com>
2024-02-20 09:22:29 +0500

    priceType / index, create/update

--- src/router/routes.js
+++ src/router/routes.js
@@ -90,6 +90,11 @@ const routes = [
     component: () => import("../pages/list/organizationBill/edit.vue"),
   },
   {
+    path: "/list/priceType",
+    name: "priceType",
+    component: () => import("../pages/list/priceType/index.vue"),
+  },
+  {
     path: "/login",
     name: "login",
     component: () => import("../pages/authentication/authentication.vue"),
