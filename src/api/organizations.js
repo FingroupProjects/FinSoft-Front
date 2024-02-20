4b8ae86e62dd9bc5206b4f41137cb35222330953
Sheroz <123716684+sheroz-18@users.noreply.github.com>
2024-02-20 16:06:51 +0300

    organization

--- src/api/organizations.js
+++ src/api/organizations.js
@@ -1,8 +1,16 @@
 import { api } from './api.js'
 
 export default {
-  getAll(data) {
-    return  api.get('/organization', data)
+  getAll(page = 1, itemPerPage = 10) {
+    return  api.get('/organization', {
+      params: {
+        page: page,
+        itemPerPage: itemPerPage
+      }
+    })
+  },
+  add() {
+    return api.post('/organization')
   },
   create(data) {
     return  api.patch(`/counterparty/${id}`, data)
@@ -10,5 +18,4 @@ export default {
   update() {
     return api.get('/counterparty')
   },
-
 }
