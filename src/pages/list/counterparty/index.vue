3e35c60049dddb2dc41b79571c9a49220ace605a
john_front_dev <jamshid200404@gmail.com>
2024-02-23 11:28:44 +0500

    fix

--- src/pages/list/counterparty/index.vue
+++ src/pages/list/counterparty/index.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
 import { useRouter } from "vue-router";
 import counterpartyApi from "../../../api/counterparty";
 import showDate from "../../../composables/date/showDate"
+import {add, addIcon, cancel, editIcon, prevIcon, showIcon} from "../../../composables/constant/buttons.js";
 
 const loading = ref(true);
 
@@ -57,36 +58,66 @@ const fetchCounterparty = async ({ page, itemsPerPage, sortBy, search }) => {
   }
 };
 </script>
+
 <template>
   <div>
-    <v-col class="d-flex flex-column ga-5">
-      <div class="d-flex w-100 justify-space-between">
-        <v-btn rounded="lg" variant="outlined" color="info" @click.prevent="$router.push('/adminPanel')">Назад</v-btn>
-        <v-btn rounded="lg" color="info" @click="$router.push('createCounterparty')">Добавить</v-btn>
+    <v-col>
+
+      <div class="d-flex justify-space-between">
+        <v-btn color="info" class="rounded-circle mb-1" size="40" @click="$router.push('/adminPanel')">
+          <v-icon color="white" size="25" >{{ prevIcon }}</v-icon>
+        </v-btn>
+        <v-btn class="rounded-circle pa-2 mb-1" color="green" size="40" @click="$router.push('createCounterparty')">
+          <v-icon size="25">{{ addIcon }}</v-icon>
+        </v-btn>
       </div>
-      <v-card class="table">
+
+      <v-card class="table mt-2">
+
         <v-card-title class="d-flex align-center pe-2">
           Список контрагентов
+          <v-spacer />
+          <v-spacer />
+          <v-spacer />
+          <v-text-field
+            v-model="search"
+            prepend-inner-icon="search"
+            clearable
+            density="compact"
+            label="Поиск..."
+            color="info"
+            rounded="lg"
+            single-line
+            flat
+            hide-details
+            variant="outlined"
+          />
 
-          <v-spacer />
-          <v-spacer />
-          <v-spacer />
-
-          <v-text-field v-model="search" prepend-inner-icon="search" density="compact" label="Поиск..." single-line flat
-            hide-details variant="outlined"></v-text-field>
         </v-card-title>
-        <v-data-table-server :items="counterparty" :headers="headers" :loading="loading"
-          items-per-page-text="Элементов на странице:" loading-text="Загрузка" no-data-text="Нет данных" :search="search"
-          @update:options="fetchCounterparty" v-model:items-per-page="paginations.per_page"
-          :items-length="paginations.total || 0" :item-value="headers.title">
+        <v-data-table-server
+          :items="counterparty"
+          :headers="headers"
+          :loading="loading"
+          items-per-page-text="Элементов на странице:"
+          loading-text="Загрузка"
+          no-data-text="Нет данных"
+          :search="search"
+          @update:options="fetchCounterparty"
+          v-model:items-per-page="paginations.per_page"
+          :items-length="paginations.total || 0"
+          :item-value="headers.title"
+        >
           <template v-slot:item.id="{ item, index }">
             <span>{{ index + 1 }}</span>
           </template>
           <template #item.icons="{ item }">
-            <v-icon color="info" class="icon mr-2" @click="pushToRename(item)">edit</v-icon>
-            <v-icon color="info" class="icon" @click="showDetail(item)">visibility</v-icon>
+            <div class="d-flex ga-1">
+              <v-icon color="info" class="icon" @click="showDetail(item)">visibility</v-icon>
+              <v-icon color="orange" class="icon" @click="pushToRename(item)">edit</v-icon>
+            </div>
           </template>
         </v-data-table-server>
+
       </v-card>
     </v-col>
   </div>
