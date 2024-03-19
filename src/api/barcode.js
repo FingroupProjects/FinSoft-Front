0513599c8b54a43b7ed1c2e6a9ace5fc9a3105c8
john_front_dev <jamshid200404@gmail.com>
2024-03-19 17:09:30 +0500

    goods

--- src/api/barcode.js
+++ src/api/barcode.js
@@ -2,15 +2,22 @@ import { api, buildParams } from "./api.js";
 
 export default {
   create(data) {
-    return api.post("/barcode", data);
+    return api.post(`/barcode`, data);
   },
-  get({ page = 1, itemsPerPage = 10, sortBy }, search = "") {
+  get({ page = 1, itemsPerPage = 100, sortBy }, search = "") {
     const params = buildParams(page, itemsPerPage, sortBy, search);
     return api.get("/barcode", { params });
+  },
+  update(id, data) {
+    return api.patch(`/barcode/${id}`, data);
   },
   getById(id, { page = 1, itemsPerPage = 10, sortBy }, search = "") {
     const params = buildParams(page, itemsPerPage, sortBy, search);
     return api.get(`/barcode/${id}`, { params });
+  },
+  getBarcodeById(id, { page = 1, itemsPerPage = 10, sortBy }, search = "") {
+    const params = buildParams(page, itemsPerPage, sortBy, search);
+    return api.get(`/get-barcode/${id}`, { params });
   },
   massDeletion(data) {
     return api.post(`/barcode/massDelete/`, data);
