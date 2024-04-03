cd1ed270433b4191886307100d0a0d1da41c28a7
rustam18-dev <rustam.radzabov8410@gmail.com>
2024-04-03 11:47:29 +0500

    procurement

---
+++ src/components/formElements/CustomAutocomplete.vue
@@ -0,0 +1,21 @@
+<script setup>
+import {FIELD_COLOR} from "../../composables/constant/colors.js"
+
+</script>
+
+<template>
+  <v-autocomplete
+      style="max-width: 250px; min-width: 250px;"
+      variant="outlined"
+      color="green"
+      rounded="lg"
+      no-data-text="Нет данных"
+      :base-color="FIELD_COLOR"
+      item-title="name"
+      item-value="id"
+  />
+</template>
+
+<style scoped>
+
+</style>
