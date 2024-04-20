edcf88dbeecc1f07518ad1b933c94c1ccefe03c4
rustam18-dev <rustam.radzabov8410@gmail.com>
2024-04-20 17:24:20 +0500

    providerOrder

--- src/pages/procurement/providerOrder/create.vue
+++ src/pages/procurement/providerOrder/create.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {computed, onMounted, reactive, ref, watch} from "vue";
+import {computed, onMounted, reactive, ref, watch, defineEmits} from "vue";
 import Icons from "../../../composables/Icons/Icons.vue";
 import CustomTextField from "../../../components/formElements/CustomTextField.vue";
 import CustomAutocomplete from "../../../components/formElements/CustomAutocomplete.vue";
@@ -7,31 +7,27 @@ import CustomCheckbox from "../../../components/checkbox/CustomCheckbox.vue";
 import showToast from "../../../composables/toast/index.js";
 import currentDate from "../../../composables/date/currentDate.js";
 import validate from "./validate.js";
-import { useRouter } from "vue-router";
+import {useRouter} from "vue-router";
 import organizationApi from "../../../api/list/organizations.js";
 import counterpartyApi from "../../../api/list/counterparty.js";
-import storageApi from "../../../api/list/storage.js";
 import cpAgreementApi from "../../../api/list/counterpartyAgreement.js";
 import currencyApi from "../../../api/list/currency.js";
-import providerApi from "../../../api/documents/providerReturn.js";
+import providerOrderApi from "../../../api/documents/providerOrder.js";
 import goodApi from "../../../api/list/goods.js";
-import { addMessage } from "../../../composables/constant/buttons.js";
-import "../../../assets/css/procurement.css";
+import {addMessage} from "../../../composables/constant/buttons.js";
 import {BASE_COLOR} from "../../../composables/constant/colors.js";
-
+import "../../../assets/css/procurement.css";
+import { useConfirmDocumentStore } from "../../../store/confirmDocument.js";
 
 const router = useRouter()
+const emits = defineEmits(['changed'])
+const confirmDocument = useConfirmDocumentStore()
 
 const form = reactive({
   date: null,
   organization: null,
-  organizations: [],
   counterparty: null,
-  counterparties: [],
   cpAgreement: null,
-  cpAgreements: [],
-  storage: null,
-  storages: [],
   saleInteger: null,
   salePercent: null,
   comment: null,
@@ -44,14 +40,13 @@ const markedID = ref([])
 const goods = ref([{
   id: 1,
   good_id: null,
-  amount: 1,
+  amount: "1",
   price: null,
 }])
 
 const organizations = ref([])
 const counterparties = ref([])
 const cpAgreements = ref([])
-const storages = ref([])
 const currencies = ref([])
 const listGoods = ref([])
 
@@ -63,32 +58,27 @@ const headers = ref([
 ])
 
 const getOrganizations = async () => {
-  const { data } = await organizationApi.get({page: 1, itemsPerPage: 100000, sortBy: 'name'});
+  const {data} = await organizationApi.get({page: 1, itemsPerPage: 100000, sortBy: 'name'});
   organizations.value = data.result.data
 }
 
 const getCounterparties = async () => {
-  const { data } = await counterpartyApi.get({page: 1, itemsPerPage: 100000, sortBy: 'name'});
+  const {data} = await counterpartyApi.get({page: 1, itemsPerPage: 100000, sortBy: 'name'});
   counterparties.value = data.result.data
 }
 
 const getCpAgreements = async () => {
-  const { data } = await cpAgreementApi.get({page: 1, itemsPerPage: 100000, sortBy: 'name'});
+  const {data} = await cpAgreementApi.get({page: 1, itemsPerPage: 100000, sortBy: 'name'});
   cpAgreements.value = data.result.data
 }
 
-const getStorages = async () => {
-  const { data } = await storageApi.get({page: 1, itemsPerPage: 100000, sortBy: 'name'});
-  storages.value = data.result.data
-}
-
 const getCurrencies = async () => {
-  const { data } = await currencyApi.get({page: 1, itemsPerPage: 100000, sortBy: 'name'});
+  const {data} = await currencyApi.get({page: 1, itemsPerPage: 100000, sortBy: 'name'});
   currencies.value = data.result.data
 }
 
 const getGoods = async () => {
-  const { data } = await goodApi.get({page: 1, itemsPerPage: 100000, sortBy: 'name'});
+  const {data} = await goodApi.get({page: 1, itemsPerPage: 100000, sortBy: 'name'});
   listGoods.value = data.result.data
 }
 
@@ -111,7 +101,7 @@ const increaseCountOfGoods = () => {
   const missingData = goods.value.some(validateItem)
   if (missingData) return
 
-  goods.value.push({id: goods.value.length + 1, good_id: null, amount: 1, price: null })
+  goods.value.push({id: goods.value.length + 1, good_id: null, amount: "1", price: null})
 }
 
 const validateItem = (item) => {
@@ -130,8 +120,8 @@ const validateItem = (item) => {
   return false;
 };
 
-const addNewProvider = async () => {
-  if (validate(form.date, form.organization, form.counterparty, form.cpAgreement, form.storage, form.currency) !== true) return
+const addNewProviderOrder = async () => {
+  if (validate(form.date, form.organization, form.counterparty, form.cpAgreement, form.currency) !== true) return
 
   const missingData = goods.value.some(validateItem)
   if (missingData) return
@@ -141,30 +131,40 @@ const addNewProvider = async () => {
     organization_id: form.organization,
     counterparty_id: form.counterparty,
     counterparty_agreement_id: form.cpAgreement,
-    storage_id: form.storage ,
     saleInteger: Number(form.saleInteger),
     salePercent: Number(form.salePercent),
     currency_id: typeof form.currency === 'object' ? form.currency.id : form.currency,
-    comment : form.comment, 
+    summa: totalPrice.value,
     goods: goods.value.map((item) => ({
       good_id: Number(item.good_id),
       amount: Number(item.amount),
       price: Number(item.price),
     }))
- }
+  }
 
-  console.log(body)
-
- try {
-   const res = await providerApi.add(body)
-   if (res.status === 201) {
-     showToast(addMessage)
-     router.push('/providerOfGoods')
-   }
- } catch (e) {
-   console.log(e)
- }
+  try {
+    const res = await providerOrderApi.add(body)
+    if (res.status === 201) {
+      showToast(addMessage)
+      router.push('/providerOrder')
+    }
+  } catch (e) {
+    console.log(e)
+  }
 }
+
+const isChanged = () => {
+  const {saleInteger, salePercent, organization, counterparty, cpAgreement, currency, date} = form;
+
+  const goodsValues = goods.value.flatMap(good => [good.good_id, good.amount, good.price]);
+
+  const cleanedGoodsValues = goodsValues.filter(val => val !== undefined);
+  const valuesToCheck = [saleInteger, salePercent, organization, counterparty, cpAgreement, currency, date, ...cleanedGoodsValues];
+
+  return valuesToCheck.every(val => val === null || val === '' || val === currentDate() || val === "1");
+}
+
+
 const totalPrice = computed(() => {
   let sum = 0
   goods.value.forEach(item => {
@@ -173,31 +173,8 @@ const totalPrice = computed(() => {
   return sum
 })
 
-const totalPriceWithSale = computed(() => {
-  let sum = 0
-  if (form.salePercent !== null) {
-      sum = totalPrice.value - (totalPrice.value * form.salePercent / 100)
-  } else {
-    goods.value.forEach(item => {
-      sum += (item.price * item.amount)
-    })
-    sum -= form.saleInteger
-  }
-
-  return sum
-})
-
-
-onMounted(() => {
-  form.date = currentDate()
-  author.value = JSON.parse(localStorage.getItem('user')).name || null
-  getOrganizations()
-  getCounterparties()
-  getCpAgreements()
-  getStorages()
-  getCurrencies()
-  getGoods()
-})
+const isSaleIntegerDisabled = computed(() => !!form.salePercent);
+const isSalePercentDisabled = computed(() => !!form.saleInteger);
 
 watch(() => form.counterparty, async (id) => {
   form.cpAgreement = null
@@ -220,9 +197,6 @@ watch(() => form.counterparty, async (id) => {
   }
 })
 
-const isSaleIntegerDisabled = computed(() => !!form.salePercent);
-const isSalePercentDisabled = computed(() => !!form.saleInteger);
-
 watch(() => form.saleInteger, (newValue) => {
   if (!newValue) {
     form.salePercent = ''
@@ -235,23 +209,45 @@ watch(() => form.salePercent, (newValue) => {
   }
 })
 
+watch(confirmDocument, () => {
+  if (confirmDocument.isUpdateOrCreateDocument) {
+    addNewProviderOrder()
+  }
+})
+
+watch([form, goods.value], () => {
+  if (!isChanged()) {
+    emits('changed', true);
+  } else {
+    emits('changed', false);
+  }
+});
+
+onMounted(() => {
+  form.date = currentDate()
+  author.value = JSON.parse(localStorage.getItem('user')).name || null
+
+  getOrganizations()
+  getCounterparties()
+  getCpAgreements()
+  getCurrencies()
+  getGoods()
+})
+
 </script>
 <template>
   <div class="document">
     <v-col>
       <div class="d-flex justify-space-between text-uppercase ">
         <div class="d-flex align-center ga-2 pe-2 ms-4">
-          <span>Возврат поставщику (создание)</span>
+          <span>Заказ поставщику (создание)</span>
         </div>
         <v-card variant="text" class="d-flex align-center ga-2">
           <div class="d-flex w-100">
-            <div class="d-flex ga-2 mt-1 me-3">
-              <Icons title="Добавить" @click="addNewProvider" name="add"/>
-              <Icons title="Скопировать" @click="" name="copy"/>
-              <Icons title="Удалить" @click="" name="delete"/>
+            <div class="d-flex ga-2 mt-1">
+              <Icons title="Добавить" @click="addNewProviderOrder" name="add"/>
             </div>
           </div>
-
         </v-card>
       </div>
     </v-col>
@@ -260,14 +256,11 @@ watch(() => form.salePercent, (newValue) => {
     <div style="background: #fff;">
       <v-col class="d-flex flex-column ga-2 pb-0">
         <div class="d-flex flex-wrap ga-4">
-          <custom-text-field disabled value="Номер" v-model="form.number"/>
+          <custom-text-field disabled value="Номер"/>
           <custom-text-field label="Дата" type="date" v-model="form.date"/>
-          <custom-autocomplete label="Организация" :items="organizations"  v-model="form.organization"/>
-          <custom-autocomplete label="Поставщик" :items="counterparties" v-model="form.counterparty"/>
+          <custom-autocomplete label="Организация" :items="organizations" v-model="form.organization"/>
+          <custom-autocomplete label="Клиент" :items="counterparties" v-model="form.counterparty"/>
           <custom-autocomplete label="Договор" :items="cpAgreements" v-model="form.cpAgreement"/>
-          <custom-autocomplete label="Склад" :items="storages" v-model="form.storage"/>
-          <custom-text-field label="Руч. скидка (сумма)" v-mask="'###'" v-model="form.saleInteger" :disabled="isSaleIntegerDisabled"/>
-          <custom-text-field label="Руч. скидка (процент)" v-mask="'###'" v-model="form.salePercent" :disabled="isSalePercentDisabled"/>
         </div>
       </v-col>
       <v-col>
@@ -276,9 +269,9 @@ watch(() => form.salePercent, (newValue) => {
             <Icons name="add" title="Добавить поле" @click="increaseCountOfGoods"/>
             <Icons name="delete" @click="decreaseCountOfGoods"/>
           </div>
-          <div class="d-flex flex-column w-100 goods">
+          <div class="d-flex flex-column w-100">
             <v-data-table
-                style="height: 78vh"
+                style="height: 50vh"
                 items-per-page-text="Элементов на странице:"
                 loading-text="Загрузка"
                 no-data-text="Нет данных"
@@ -299,24 +292,24 @@ watch(() => form.salePercent, (newValue) => {
                 <tr :key="index">
                   <td>
                     <CustomCheckbox
-                      v-model="markedID"
-                      @change="lineMarking(item)"
-                      :checked="markedID.includes(item.id)"
+                        v-model="markedID"
+                        @change="lineMarking(item)"
+                        :checked="markedID.includes(item.id)"
                     >
-                      <span>{{ index + 1}}</span>
+                      <span>{{ index + 1 }}</span>
                     </CustomCheckbox>
                   </td>
-                  <td>
-                    <custom-autocomplete v-model="item.good_id" :items="listGoods" min-width="150" />
+                  <td style="width: 40%;">
+                    <custom-autocomplete v-model="item.good_id" :items="listGoods" min-width="150" max-width="100%"/>
                   </td>
                   <td>
-                    <custom-text-field v-model="item.amount" v-mask="'########'" min-width="50" max-width="90" />
+                    <custom-text-field v-model="item.amount" v-mask="'########'" min-width="50"/>
                   </td>
                   <td>
-                    <custom-text-field v-model="item.price" v-mask="'##########'" min-width="80" max-width="110"/>
+                    <custom-text-field v-model="item.price" v-mask="'##########'" min-width="80"/>
                   </td>
                   <td>
-                    <custom-text-field readonly :value="item.amount * item.price"  min-width="100" max-width="110"/>
+                    <custom-text-field readonly :value="item.amount * item.price" min-width="100"/>
                   </td>
                 </tr>
               </template>
@@ -325,13 +318,13 @@ watch(() => form.salePercent, (newValue) => {
         </div>
         <div class="d-flex justify-space-between w-100 mt-2 bottomField">
           <div class="d-flex ga-10">
-            <custom-text-field readonly :value="author"  min-width="140" max-width="110"/>
+            <custom-text-field readonly :value="author" min-width="110"/>
             <custom-text-field label="Комментарий" v-model="form.comment" min-width="310"/>
           </div>
           <div class="d-flex ga-6">
-            <custom-text-field readonly :value="'Сумма со скидкой: ' + totalPriceWithSale" min-width="180" />
-            <custom-text-field readonly  :value="'Сумма без скидки: ' + totalPrice" min-width="180" max-width="110"/>
-            <custom-autocomplete v-model="form.currency" label="Валюта" :items="currencies" min-width="110" max-width="110" />
+            <custom-text-field readonly :value="'Сумма без скидки: ' + totalPrice" min-width="180" max-width="110"/>
+            <custom-autocomplete v-model="form.currency" label="Валюта" :items="currencies" min-width="110"
+                                 max-width="110"/>
           </div>
         </div>
       </v-col>
@@ -340,5 +333,6 @@ watch(() => form.salePercent, (newValue) => {
 </template>
 
 <style scoped>
+@import "../../../assets/css/procurement.css";
 
 </style>
