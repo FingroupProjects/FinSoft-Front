9b058d9a0ec9d943e02313e949a998ddc0051899
john_front_dev <jamshid200404@gmail.com>
2024-05-07 08:19:58 +0500

    amount good

--- src/pages/procurement/procurementOfGoods/create.vue
+++ src/pages/procurement/procurementOfGoods/create.vue
@@ -118,7 +118,6 @@ const getGoods = async (good_storage_id, good_organization_id) => {
     good_organization_id
   );
   listGoods.value = data.result.data;
-  console.log(data);
 };
 
 const decreaseCountOfGoods = () => {
@@ -472,6 +471,7 @@ onMounted(() => {
                       :items="listGoods"
                       min-width="150"
                       max-width="100%"
+                      :isAmount="true"
                     />
                   </td>
                   <td>
