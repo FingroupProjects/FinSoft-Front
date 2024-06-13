99f8e1f487237fdddfeaaefa39ef253666f4d63b
john_front_dev <jamshid200404@gmail.com>
2024-06-13 15:29:49 +0500

    fix PKO

--- src/pages/cash/moneyComing/index.vue
+++ src/pages/cash/moneyComing/index.vue
@@ -2,6 +2,8 @@
 import { onMounted, ref, watch } from "vue";
 import { useRouter } from "vue-router";
 import showToast from "../../../composables/toast/index.js";
+import CustomFilterAutocomplete from "../../../components/formElements/CustomFilterAutocomplete.vue";
+import CustomFilterTextField from "../../../components/formElements/CustomFilterTextField.vue";
 import Icons from "../../../composables/Icons/Icons.vue";
 import CustomTextField from "../../../components/formElements/CustomTextField.vue";
 import CustomAutocomplete from "../../../components/formElements/CustomAutocomplete.vue";
@@ -9,6 +11,7 @@ import CustomCheckbox from "../../../components/checkbox/CustomCheckbox.vue";
 import {
   BASE_COLOR,
   FIELD_OF_SEARCH,
+  TITLE_COLOR,
 } from "../../../composables/constant/colors.js";
 import {
   removeMessage,
@@ -16,9 +19,12 @@ import {
   ErrorSelectMessage,
   restoreMessage,
 } from "../../../composables/constant/buttons.js";
+import { useFilterCanvasVisible } from "../../../store/canvasVisible.js";
+import FilterCanvas from "../../../components/canvas/filterCanvas.vue";
 import debounce from "lodash.debounce";
 import userApi from "../../../api/list/user.js";
 import currencyApi from "../../../api/list/currency.js";
+import Button from "../../../components/button/button.vue";
 import organizationApi from "../../../api/list/organizations.js";
 import clientPaymentApi from "../../../api/documents/cashRegister.js";
 ("../../../api/documents/sale.js");
@@ -52,7 +58,8 @@ const cashRegisters = ref([]);
 
 const filterForm = ref({
   sum: null,
-  date: null,
+  startDate: null,
+  endDate: null,
   author_id: null,
   currency_id: null,
   operationType: null,
@@ -71,6 +78,37 @@ const headers = ref([
   { title: "Сумма", key: "currency.name" },
   { title: "Валюта", key: "currency.name" },
   { title: "Автор", key: "sender.name" },
+]);
+
+const headerButtons = ref([
+  {
+    name: "create",
+    function: () => router.push({ name: "moneyComingCreate" }),
+  },
+  {
+    name: "copy",
+    function: async () => {
+      if (markedID.value.length !== 1) {
+        return showToast(selectOneItemMessage, "warning");
+      }
+
+      try {
+        const res = await copyDocument.copy(markedID.value[0]);
+        if (res.status === 200) {
+          showToast(copyMessage);
+          window.open(`/procurementOfGoods/${res.data.result.id}`, "_blank");
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    },
+  },
+  {
+    name: "delete",
+    function: () => {
+      compute({});
+    },
+  },
 ]);
 
 const getSellingGoods = async ({ page, itemsPerPage, sortBy, search }) => {
@@ -183,6 +221,7 @@ const closeFilterModal = async ({ page, itemsPerPage, sortBy, search }) => {
   filterModal.value = {};
   cleanFilterForm();
   await getSellingGoods({ page, itemsPerPage, sortBy, search });
+  useFilterCanvasVisible().closeFilterCanvas();
 };
 
 const cleanFilterForm = () => {
@@ -237,14 +276,14 @@ const getTypes = async () => {
 const getCurrencies = async () => {
   try {
     const { data } = await currencyApi.get({ page: 1, itemsPerPage: 100000 });
-    currencies.value = data.result.data
+    currencies.value = data.result.data;
   } catch (e) {}
 };
 
 const getAuthors = async () => {
   const { data } = await userApi.getAuthors();
-  authors.value = data.result.data
-}
+  authors.value = data.result.data;
+};
 
 onMounted(async () => {
   await Promise.all([
@@ -258,205 +297,196 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div>
-    <v-col>
-      <div class="d-flex justify-space-between text-uppercase">
-        <div class="d-flex align-center ga-2 pe-2 ms-4">
-          <span>ПКО</span>
-        </div>
-        <v-card variant="text" min-width="350" class="d-flex align-center ga-2">
-          <div class="d-flex w-100">
-            <div class="d-flex ga-2 mt-1 me-3">
-              <Icons
-                title="Добавить"
-                @click="$router.push('/moneyComingCreate')"
-                name="add"
-              />
-              <Icons title="Скопировать" name="copy" />
-              <Icons title="Удалить" @click="compute" name="delete" />
-            </div>
-
-            <div class="w-100">
-              <v-text-field
-                v-model="search"
-                prepend-inner-icon="search"
-                density="compact"
-                label="Поиск..."
-                variant="outlined"
-                :color="BASE_COLOR"
-                rounded="lg"
-                :base-color="FIELD_OF_SEARCH"
-                clear-icon="close"
-                hide-details
-                single-line
-                :append-inner-icon="search ? 'close' : ''"
-                @click:append-inner="search = ''"
-                flat
-              ></v-text-field>
-            </div>
-          </div>
-          <div class="filterElement">
-            <Icons
-              name="filter"
-              title="фильтр"
-              @click="filterModal = true"
-              class="mt-1"
-            />
-            <span v-if="count !== 0" class="countFilter">{{ count }}</span>
-          </div>
-        </v-card>
+  <div class="pa-4">
+    <div class="d-flex justify-space-between">
+      <div class="d-flex align-center ga-2 pe-2 ms-4">
+        <span :style="{ color: TITLE_COLOR, fontSize: '22px' }">Пко</span>
       </div>
-      <v-card class="mt-2 table">
-        <v-data-table-server
-          style="height: 78vh"
-          items-per-page-text="Элементов на странице:"
-          loading-text="Загрузка"
-          no-data-text="Нет данных"
-          v-model:items-per-page="paginations.per_page"
-          :loading="loading"
-          :headers="headers"
-          :items-length="paginations.total || 0"
-          :items="moneyComing"
-          :item-value="headers.title"
-          :search="debounceSearch"
-          v-model="markedID"
-          @update:options="getSellingGoods"
-          page-text="{0}-{1} от {2}"
-          :items-per-page-options="[
-            { value: 25, title: '25' },
-            { value: 50, title: '50' },
-            { value: 100, title: '100' },
-          ]"
-          show-select
-          fixed-header
-          hover
-        >
-          <template v-slot:item="{ item, index }">
-            <tr
-              @mouseenter="hoveredRowIndex = index"
-              @mouseleave="hoveredRowIndex = null"
-              @dblclick="$router.push(`/moneyComingEdit/${item.id}`)"
-              :class="{ 'bg-grey-lighten-2': markedID.includes(item.id) }"
-            >
-              <td>
-                <template
-                  v-if="hoveredRowIndex === index || markedID.includes(item.id)"
-                >
-                  <CustomCheckbox
-                    v-model="markedID"
-                    :checked="markedID.includes(item.id)"
-                    @change="lineMarking(item)"
-                  >
-                    <span>{{ index + 1 }}</span>
-                  </CustomCheckbox>
-                </template>
-                <template v-else>
-                  <div class="d-flex align-center">
-                    <Icons
-                      style="margin-right: 10px; margin-top: 4px"
-                      :name="item.deleted_at === null ? 'valid' : 'inValid'"
-                    />
-                    <span>{{ index + 1 }}</span>
-                  </div>
-                </template>
-              </td>
-              <td>{{ item.doc_number }}</td>
-              <td>{{ showDate(item.date) }}</td>
-              <td>{{ item.cashRegister ? item.cashRegister.name : "" }}</td>
-              <td>{{ item.organization.name }}</td>
-              <td>{{ item.operationType.name }}</td>
-              <td>{{ item.counterparty ? item.counterparty.name : "" }}</td>
-              <td>{{ item.sum }}</td>
-              <td>{{ item.currency }}</td>
-              <td>{{ item.author.name }}</td>
-            </tr>
-          </template>
-        </v-data-table-server>
-      </v-card>
-
-      <v-card>
-        <v-dialog
-          persistent
-          class="mt-2 pa-2"
-          v-model="filterModal"
-          @keyup.esc="closeFilterModal"
-        >
-          <v-card
-            :style="`border: 2px solid ${BASE_COLOR}`"
-            min-width="450"
-            class="d-flex pa-5 pt-2 justify-center flex-column mx-auto my-0"
-            rounded="xl"
+      <div class="d-flex justify-end ga-2">
+        <div class="d-flex w-100 justify-end mb-3">
+          <div class="d-flex ga-2 position-relative">
+            <Button
+              v-for="(button, idx) in headerButtons"
+              :name="button.name"
+              :key="idx"
+              @click="button.function"
+            />
+          </div>
+        </div>
+        <div class="custom_search">
+          <v-text-field
+            style="width: 190px"
+            v-model="search"
+            prepend-inner-icon="search"
+            density="compact"
+            label="Поиск..."
+            variant="outlined"
+            :color="BASE_COLOR"
+            rounded="lg"
+            :base-color="FIELD_OF_SEARCH"
+            clear-icon="close"
+            hide-details
+            single-line
+            :append-inner-icon="search ? 'close' : ''"
+            @click:append-inner="search = ''"
+            flat
+          />
+        </div>
+        <div class="mt-2 filterElement">
+          <Icons
+            name="filter"
+            title="фильтр"
+            @click="useFilterCanvasVisible().toggleFilterCanvas()"
+          />
+          <span v-if="count !== 0" class="countFilter">{{ count }}</span>
+        </div>
+      </div>
+    </div>
+    <v-card class="table">
+      <v-data-table-server
+        style="height: calc(100vh - 150px)"
+        items-per-page-text="Элементов на странице:"
+        loading-text="Загрузка"
+        no-data-text="Нет данных"
+        v-model:items-per-page="paginations.per_page"
+        :loading="loading"
+        :headers="headers"
+        :items-length="paginations.total || 0"
+        :items="moneyComing"
+        :item-value="headers.title"
+        :search="debounceSearch"
+        v-model="markedID"
+        @update:options="getSellingGoods"
+        page-text="{0}-{1} от {2}"
+        :items-per-page-options="[
+          { value: 25, title: '25' },
+          { value: 50, title: '50' },
+          { value: 100, title: '100' },
+        ]"
+        show-select
+        fixed-header
+        hover
+      >
+        <template v-slot:item="{ item, index }">
+          <tr
+            @mouseenter="hoveredRowIndex = index"
+            @mouseleave="hoveredRowIndex = null"
+            @dblclick="$router.push(`/moneyComingEdit/${item.id}`)"
+            :class="{ 'bg-grey-lighten-2': markedID.includes(item.id) }"
           >
-            <div class="d-flex justify-space-between align-center mb-2">
-              <span>Фильтр</span>
-            </div>
-            <v-form class="d-flex w-100">
-              <v-row class="w-100">
-                <v-col class="d-flex flex-column w-100 ga-4">
-                  <div class="d-flex ga-2 w-100">
-                    <custom-text-field
-                      label="Дата"
-                      type="date"
-                      v-model="filterForm.date"
-                    />
-                    <custom-text-field label="Сумма" v-model="filterForm.sum" />
-                  </div>
-                  <div class="d-flex ga-2">
-                    <custom-autocomplete
-                      label="Организация"
-                      :items="organizations"
-                      v-model="filterForm.organization_id"
-                    />
-                    <custom-autocomplete
-                      label="Касса"
-                      :items="cashRegisters"
-                      v-model="filterForm.cash_register_id"
-                    />
-                  </div>
-                  <div class="d-flex ga-2">
-                    <custom-autocomplete
-                      label="Тип операции"
-                      :items="typeOperations"
-                      item-title="title_ru"
-                      item-value="id"
-                      v-model="filterForm.operationType"
-                    />
-                    <custom-autocomplete
-                      label="Плательщик"
-                      :items="counterparties"
-                      v-model="filterForm.counterparty_id"
-                    />
-                  </div>
-                  <div class="d-flex ga-2">
-                    <custom-autocomplete
-                      label="Валюта"
-                      :items="currencies"
-                      v-model="filterForm.currency_id"
-                    />
-                    <custom-autocomplete
-                      label="Автор"
-                      :items="authors"
-                      v-model="filterForm.author_id"
-                    />
-                  </div>
-                  <div class="d-flex justify-end ga-2">
-                    <v-btn color="red" class="btn" @click="closeFilterModal"
-                      >сбросить</v-btn
-                    >
-                    <v-btn
-                      :color="BASE_COLOR"
-                      class="btn"
-                      @click="getSellingGoods"
-                      >применить</v-btn
-                    >
-                  </div>
-                </v-col>
-              </v-row>
-            </v-form>
-          </v-card>
-        </v-dialog>
-      </v-card>
-    </v-col>
+            <td>
+              <template
+                v-if="hoveredRowIndex === index || markedID.includes(item.id)"
+              >
+                <CustomCheckbox
+                  v-model="markedID"
+                  :checked="markedID.includes(item.id)"
+                  @change="lineMarking(item)"
+                >
+                  <span>{{ index + 1 }}</span>
+                </CustomCheckbox>
+              </template>
+              <template v-else>
+                <div class="d-flex align-center">
+                  <Icons
+                    style="margin-right: 10px; margin-top: 4px"
+                    :name="item.deleted_at === null ? 'valid' : 'inValid'"
+                  />
+                  <span>{{ index + 1 }}</span>
+                </div>
+              </template>
+            </td>
+            <td>{{ item.doc_number }}</td>
+            <td>{{ showDate(item.date) }}</td>
+            <td>{{ item.cashRegister ? item.cashRegister.name : "" }}</td>
+            <td>{{ item.organization.name }}</td>
+            <td>{{ item.operationType.name }}</td>
+            <td>{{ item.counterparty ? item.counterparty.name : "" }}</td>
+            <td>{{ item.sum }}</td>
+            <td>{{ item.currency }}</td>
+            <td>{{ item.author.name }}</td>
+          </tr>
+        </template>
+      </v-data-table-server>
+    </v-card>
+    <filter-canvas>
+      <div class="d-flex flex-column ga-2 w-100">
+        <custom-filter-text-field
+          label="Дата"
+          min-width="106"
+          class="date"
+          clearable
+          type="datetime-local"
+          v-model="filterForm.date"
+        />
+        <custom-filter-text-field
+          label="По"
+          type="datetime-local"
+          class="date"
+          min-width="106"
+          clearable
+          v-model="filterForm.endDate"
+        />
+        <custom-filter-autocomplete
+          min-width="106"
+          label="Организация"
+          :items="organizations"
+          v-model="filterForm.organization_id"
+        />
+        <custom-filter-autocomplete
+          min-width="106"
+          label="Касса"
+          :items="cashRegisters"
+          v-model="filterForm.cash_register_id"
+        />
+        <custom-filter-autocomplete
+          min-width="106"
+          label="Тип операции"
+          :items="typeOperations"
+          item-title="title_ru"
+          item-value="id"
+          v-model="filterForm.operationType"
+        />
+        <custom-filter-autocomplete
+          min-width="106"
+          label="Плательщик"
+          :items="counterparties"
+          v-model="filterForm.counterparty_id"
+        />
+        <custom-filter-autocomplete
+          min-width="106"
+          label="Валюта"
+          :items="currencies"
+          v-model="filterForm.currency_id"
+        />
+        <div class="d-flex ga-2">
+          <custom-filter-text-field label="Сумма" v-model="filterForm.sum" />
+          <custom-filter-autocomplete
+            label="Автор"
+            :items="authors"
+            v-model="filterForm.author_id"
+          />
+        </div>
+        <div class="d-flex justify-end ga-2">
+          <div class="d-flex ga-2" style="margin-right: -6%">
+            <v-btn color="red" class="btn" @click="closeFilterModal"
+              >сбросить</v-btn
+            >
+            <v-btn
+              :color="BASE_COLOR"
+              class="btn"
+              @click="
+                () => {
+                  getSellingGoods();
+                  useFilterCanvasVisible().closeFilterCanvas();
+                }
+              "
+              >применить
+            </v-btn>
+          </div>
+        </div>
+      </div>
+    </filter-canvas>
   </div>
 </template>
 
---
+++ src/pages/list/counterparty/map.vue
@@ -0,0 +1,109 @@
+<script>
+import { ref, onMounted } from "vue";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import "leaflet-routing-machine";
+import L from "leaflet";
+import counterpartyApi from "../../../api/list/counterparty";
+import { useRoute } from 'vue-router';
+
+export default {
+  name: "MapComponent",
+  components: {
+    LMap,
+    LTileLayer,
+  },
+  setup() {
+    const map = ref(null);
+    const route = useRoute();
+    const zoom = ref(3);
+    const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+    const attribution =
+      '&copy; <a href="https://routing.openstreetmap.de/about.html">OpenStreetMap</a> contributors';
+    const locations = ref([]);
+
+    const getLocation = async () => {
+      try {
+        const { data: { result } } = await counterpartyApi.getLocation(route.params.id);
+        console.log(result);
+        locations.value = result.map((location) => [
+          location.location.lat,
+          location.location.lon,
+        ]);
+        setTimeout(() => {
+          calculateRoutes();
+        }, 1000);
+      } catch (error) {
+        console.error("Error fetching locations:", error);
+      }
+    };
+
+    const createLocation = async () => {
+      try {
+        const body = {
+          location: {
+            lat: 40.305437,
+            lon: 69.649052,
+          },
+          date: "2024-04-04 12:12:12",
+        };
+        const response = await counterpartyApi.createLocation(body);
+        console.log(response);
+      } catch (error) {
+        console.error("Error fetching locations:", error);
+      }
+    };
+
+    const calculateRoutes = () => {
+      const mapInstance = map.value.leafletObject;
+      if (!mapInstance) {
+        console.error("Map object is not initialized yet.");
+        return;
+      }
+
+      const routingControl = L.Routing.control({
+        waypoints: locations.value.map((loc) => L.latLng(loc[0], loc[1])),
+        routeWhileDragging: false,
+        show: false,
+        addWaypoints: false,
+        createMarker: function (i, waypoint) {
+          const marker = L.marker(waypoint.latLng, {
+            draggable: false,
+          });
+          marker.bindPopup(`Маршрут: ${i}`).openPopup();
+          return marker;
+        },
+        lineOptions: { styles: [{ color: "#6FA1EC", weight: 4 }] },
+        show: false,
+      }).addTo(mapInstance);
+    };
+
+    onMounted(() => {
+      getLocation();
+      // createLocation();
+    });
+
+    return {
+      map,
+      zoom,
+      tileUrl,
+      attribution,
+    };
+  },
+};
+</script>
+
+<template>
+  <div style="position: relative;">
+    <l-map
+      ref="map"
+      :zoom="zoom"
+      :center="[46.41322, 41.219482]"
+      style="height: 100vh"
+    >
+      <l-tile-layer :url="tileUrl" :attribution="attribution"></l-tile-layer>
+    </l-map>
+    <div style="position: absolute; z-index: 9999;">
+      <v-btn>qdegregf</v-btn>
+    </div>
+  </div>
+</template>
